1d3d6412531288348058e64091e4d5418c9fac60
unknown <sayalija@Insayalija.corporate.thoughtworks.com>
2014-02-17 18:44:25 +0530

    Shuffle algorithm added

---
+++ src/com/tw/uno/lib/Shuffle.java
@@ -0,0 +1,13 @@
+package com.tw.uno.lib;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * shuffle List
+ */
+public class Shuffle {
+    Shuffle(List elements){
+        Collections.shuffle(elements);
+    }
+}
