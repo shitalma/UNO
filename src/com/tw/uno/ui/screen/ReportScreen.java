32dc5ae9cb9c08f2f3642055a2cf993bfefbca8b
ANU <aniket@sabkuch.com>
2014-02-18 10:43:47 +0530

    #2001-report screen created

---
+++ src/com/tw/uno/ui/screen/ReportScreen.java
@@ -0,0 +1,39 @@
+package com.tw.uno.ui.screen;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class ReportScreen extends JFrame {
+    DefaultTableModel model;
+    JTable table;
+    String col[] = {"Player", "NumberOfCards", "Points"};
+
+    public static void main(String args[]) {
+        new ReportScreen().displayReportTable();
+    }
+
+    public void displayReportTable() {
+
+        model = new DefaultTableModel(col, 2);
+        table = new JTable(model) {
+            @Override
+            public boolean isCellEditable(int arg0, int arg1) {
+                return false;
+            }
+        };
+        JScrollPane pane = new JScrollPane(table);
+
+        table.setValueAt("Uday", 0, 0);
+        table.setValueAt(5,0,1);
+        table.setValueAt(30,0,2);
+
+        add(pane);
+        setVisible(true);
+        setSize(500, 400);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
