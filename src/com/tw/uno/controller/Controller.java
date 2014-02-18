394cd716e3bc153abb382435d421074e75faaf6e
kavitama <jadhavkavita206@gmail.com>
2014-02-18 19:33:59 +0530

    controller conecting MyCards to Player

--- src/com/tw/uno/controller/Controller.java
+++ src/com/tw/uno/controller/Controller.java
@@ -1,8 +1,28 @@
 package com.tw.uno.controller;
 
+import com.tw.uno.lib.Pile;
+import com.tw.uno.lib.Player;
+import com.tw.uno.lib.card.Card;
+import com.tw.uno.lib.card.CardGenerator;
+
 /**
  * Connect Library to User Interface
- * 
  */
+
 public class Controller {
+    private Pile pile;
+    private Player player;
+
+    public Controller(Player player){
+        this.player = player;
+        this.pile = new Pile();
+    }
+    public void onCardPlace(Card card) {
+        pile.onCardPaced(card);
+        player.onCardPaced(card);
+    }
+
+    public Card createCard(String color, String value) {
+        return new CardGenerator().createCard(color,value);
+    }
 }
