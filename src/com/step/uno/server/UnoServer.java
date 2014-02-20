0467da14fefedc83a5ed62bff11ce43c3dae01b5
udaykant <udaykant@thoughtworks.com>
2014-02-20 15:58:34 +0530

    1001:Players are able to join the server and on all players joining they receive a snapshot

--- src/com/step/uno/server/UnoServer.java
+++ src/com/step/uno/server/UnoServer.java
@@ -1,11 +1,10 @@
 package com.step.uno.server;
 
-import com.step.communication.factory.CommunicationFactory;
-import com.step.uno.server.controller.GameMasterController;
+import com.step.uno.server.view.CreateGameScreen;
 
 public class UnoServer{
     public static void main(String[] args) {
-        GameMasterController controller = new GameMasterController(1, 1, new CommunicationFactory());
-        controller.waitForConnections();
+
+        new CreateGameScreen();
     }
 }
