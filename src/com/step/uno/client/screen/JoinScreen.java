ee0cf137efcf73ef6445d0e111047d49031205d2
udaykant <udaykant@thoughtworks.com>
2014-02-21 11:19:37 +0530

    Join Screen updated

--- src/com/step/uno/client/screen/JoinScreen.java
+++ src/com/step/uno/client/screen/JoinScreen.java
@@ -9,7 +9,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
 public class JoinScreen extends JFrame implements JoinGameView{
     JPanel loginPanel;
@@ -134,6 +133,4 @@ public class JoinScreen extends JFrame implements JoinGameView{
 
         setVisible(true);
     }
-
-
 }
